77211e980fd8b220f741b0a8333a4fb0e7716f04
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-08 16:44:41 +0530

    data types

---
+++ DataTypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num=0;
+    float myNUm=0.0;
+    double myDoubleNum = 9.98;   
+    char myLetter = 'D';         
+    bool myBoolean = true;      
+    string myText = "Hello"; 
+    char a=65;
+    cout<<num<<endl;
+    cout<<myNUm<<endl;
+    cout<<myDoubleNum<<endl;
+    cout<<myLetter<<endl;
+    cout<<myBoolean<<endl;
+    cout<<myText<<endl;
+    cout<<a;
+    return 0;
+}
