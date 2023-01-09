afa3825b84e703b98e909ba1f13fd7f3369f4e08
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-09 09:30:51 +0530

    booleans

---
+++ Booleans.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool isCodingFun=true;
+    bool isTooCold=false;
+    cout<<isCodingFun<<endl;
+    cout<<isTooCold<<endl;
+    return 0;
+}
