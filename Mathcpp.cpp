db8d3e1f81b78d0da12977e95aa63ccaac7f5ede
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-09 09:25:01 +0530

    math

---
+++ Mathcpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+    int a=10;
+    int b=20;
+    cout<<max(a,b)<<endl;
+    cout<<min(a,b)<<endl;
+    cout<<sqrt(a)<<endl;
+    return 0;
+}
