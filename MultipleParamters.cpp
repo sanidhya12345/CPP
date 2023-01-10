c614f9b6c235c5444c8d40866e895b5b8b086ece
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-10 10:42:22 +0530

    multiple paramters

---
+++ MultipleParamters.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void myfunc(string firstname,string lastname){
+    cout<<firstname+" "+lastname<<endl;
+}
+int main()
+{
+    myfunc("sanidhya","varshney");
+    return 0;
+}
